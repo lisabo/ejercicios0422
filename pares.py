e8d3fb21f3997eb203cb604505e475b50d80e59c
usuario <usuario@DELL-279313.(none)>
2013-04-22 17:29:24 -0600

    Ejercicios

---
+++ pares.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+lista2 = range(0,101)
+
+listapares = []
+
+listaimpares = []
+
+for numero in lista2:
+    if numero % 2 == 0:
+        listapares.append(numero)
+    else:
+        listaimpares.append(numero)
+
+
+
+
